1636b13ce4c55a335d8947affffc7b2246ff1d1b
AngryCaffeine <38491139+AngryCaffeine@users.noreply.github.com>
2024-01-24 02:00:04 -0800

    Update logdel.bat

--- logdel.bat
+++ logdel.bat
@@ -1,20 +1,22 @@
 @echo off
+REM This Windows batch script will clear all Windows event logs using WEVTUTIL
+REM @AngryCaffeine https://github.com/AngryCaffeine
 color 1f
-:: BatchGotAdmin Ben Gripka https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+REM BatchGotAdmin Ben Gripka https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
 :-------------------------------------
-REM  --> Check for permissions
+REM  Check for permissions
     IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
 >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
-REM --> If error flag set, we do not have admin.
+REM If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
 ) else ( goto gotAdmin )
-
+REM Ask for Administrator rights
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params= %*
@@ -28,9 +30,9 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------  
-set ch=%cd%
-cd %ch%
-    call :MsgBox "Delete All Event Logs?"  "VBYesNo+VBQuestion" "Continue ?"
+REM Change directory back and ask for confirmation
+cd /d %~dp0
+    call :_MsgBox "Delete All Event Logs?"  "VBYesNo+VBQuestion" "Continue ?"
     if errorlevel 7 (
         goto _end
     ) else if errorlevel 6 (
@@ -38,28 +40,41 @@ cd %ch%
     )
 :_strt
 echo Clearing Event Logs Please wait...
+echo This might take a few minutes...
+REM Delete old log file
 if exist log.txt (
   del log.txt
 )
-setlocal enabledelayedexpansion
-set log=log.txt
-set b=WEVTUTIL CL
-for /f %%a in ('WEVTUTIL EL') do ( 
-!b! "%%a">>!log! 2>&1
+REM Just in case the script exited prematurely 
+if exist tmp.vbs (
+  del tmp.vbs
 )
+REM WEVTUTIL EL lists the names of Windows event logs. WEVTUTIL CL Clears the Log
+setlocal enabledelayedexpansion
+set _log=log.txt
+set _b=WEVTUTIL CL
+for /f %%a in ('WEVTUTIL EL') do ( 
+!_b! "%%a">>!_log! 2>&1
+)
+REM VB msgbox to signal the task is complete
 cls
-> tmp.vbs ECHO WScript.Echo^( "COMPLETE" ^& vbCrLf ^& "Check log.txt For Any Errors" ^)
-WSCRIPT.EXE tmp.vbs
-DEL tmp.vbs
-color
-endlocal
-goto :EOF
-:MsgBox
+call :_okBox
+goto _end
+REM Yes or No Message Box
+:_MsgBox
     setlocal enableextensions
     set "tempFile=%temp%\%~nx0.%random%%random%%random%vbs.tmp"
     >"%tempFile%" echo(WScript.Quit msgBox("%~1",%~2,"%~3") & cscript //nologo //e:vbscript "%tempFile%"
     set "exitCode=%errorlevel%" & del "%tempFile%" >nul 2>nul
     endlocal & exit /b %exitCode%
+REM OK Message box
+:_okBox
+setlocal enableextensions
+set "tempFile=%temp%\%~nx0.%random%%random%%random%vbs.tmp"
+>"%tempFile%" echo(WScript.Quit msgBox("Check log.txt For Any Errors",vbOKOnly+vbInformation, "COMPLETE") & cscript //nologo //e:vbscript "%tempFile%"
+del "%tempFile%" >nul 2>nul
+endlocal & exit /b
+REM Change terminal back to default Clear screen and exit program
 :_end
 color
 cls
